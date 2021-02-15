459e1addbca193acd1858a79df4f479ec33ee6ef
ali-hajeb <ali.hajeb@gmail.com>
2021-02-15 21:45:25 +0330

    Fixed the game buttons (scissor, paper and rock) & added some basic styles

--- src/Context/userAuth.js
+++ src/Context/userAuth.js
@@ -4,6 +4,7 @@ const userContext = createContext({socket: null, user: {  userName: null,
                                             userCode: null,
                                             opponent: null,
                                             round: null,
+                                            gameNumber: null,
                                             setUserName: () => {}, 
                                             setUserCode: () => {},
                                             setUserLoggedIn: () => {}}});
