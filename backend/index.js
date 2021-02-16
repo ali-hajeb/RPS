5fa59d212d5259bf3bf86c2e069e6f9a567f2c67
ali-hajeb <ali.hajeb@gmail.com>
2021-02-16 12:28:07 +0330

    heil

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 //vali man hamchenan kos mikham
 //kir too "Samane" github
+//heil
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http, {
